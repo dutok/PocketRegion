342278da4167d0197977522665525272e8ab0e9d
Dutok <admin@axim.io>
2014-07-22 18:13:41 -0400

    Adding in test commands

--- src/PocketRegion/PocketRegion.php
+++ src/PocketRegion/PocketRegion.php
@@ -8,12 +8,21 @@
 namespace PocketRegion;
 
 
+use pocketmine\command\CommandExecutor;
+use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
 class PocketRegion extends PluginBase{
+
+    /** @var Commands */
+    protected $listener;
+
+    public $plugin;
+
     public function onEnable(){
         $this->getLogger()->info(TextFormat::GREEN . "PocketRegion has been enabled!");
+        $this->listener = new Commands($this);
     }
 
     public function onDisable(){
